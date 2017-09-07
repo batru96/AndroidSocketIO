76f14c69999c0d1a9559b17c26c9cd4cf0a7ca46
BaTru <gietbarca@gmail.com>
2017-09-07 08:29:12 +0700

    Message

--- app/src/main/java/com/example/gietb/android_tonghopsocketio/MainActivity.java
+++ app/src/main/java/com/example/gietb/android_tonghopsocketio/MainActivity.java
@@ -3,30 +3,40 @@ package com.example.gietb.android_tonghopsocketio;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import io.socket.client.IO;
 import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static Socket socket;
+    public static Socket mSocket;
+    private String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         try {
-            socket = IO.socket("http://192.168.20:118:3000/");
+            mSocket = IO.socket("http://192.168.20.100:3000/");
         } catch (URISyntaxException e) {
             e.printStackTrace();
         }
+        mSocket.connect();
+
+        mSocket.on("SERVER_SEND_THAT_BAI", dangNhapThatBaiListener);
+        mSocket.on("SERVER_SEND_THANH_CONG", dangNhapThanhCongListener);
 
         Button btnDangNhap = findViewById(R.id.button);
         final EditText edtName = findViewById(R.id.inputEdit);
@@ -37,9 +47,38 @@ public class MainActivity extends AppCompatActivity {
                 if (edtName.getText().toString().trim().equals("")) {
                     Toast.makeText(MainActivity.this, "NHU NOI", Toast.LENGTH_SHORT).show();
                 } else {
-                    startActivity(new Intent(MainActivity.this, NoiDungActivity.class));
+                    name = edtName.getText().toString();
+                    mSocket.emit("USER_SEND_NAME", name);
                 }
             }
         });
     }
+
+    private Socket.Listener dangNhapThanhCongListener = new Emitter.Listener() {
+        @Override
+        public void call(final Object... args) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    JSONArray namesArray = (JSONArray) args[0];
+                    Intent intent = new Intent(MainActivity.this, NoiDungActivity.class);
+                    intent.putExtra("DANH_SACH", namesArray.toString());
+                    intent.putExtra("TEN", name);
+                    startActivity(intent);
+                }
+            });
+        }
+    };
+
+    private Socket.Listener dangNhapThatBaiListener = new Emitter.Listener() {
+        @Override
+        public void call(Object... args) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(MainActivity.this, "USER_DA_TON_TAI", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    };
 }
--- app/src/main/java/com/example/gietb/android_tonghopsocketio/NoiDungActivity.java
+++ app/src/main/java/com/example/gietb/android_tonghopsocketio/NoiDungActivity.java
@@ -1,18 +1,68 @@
 package com.example.gietb.android_tonghopsocketio;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import io.socket.emitter.Emitter;
 
 public class NoiDungActivity extends AppCompatActivity {
+
+    private ListView lvNames;
+    private ArrayList<String> names;
+    private ArrayAdapter<String> adapterNames;
+
+    private ListView lvMessage;
+    private ArrayList<String> messages;
+    private ArrayAdapter<String> adapterMessages;
+
+    private String userName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_noi_dung);
         Button btnBack = findViewById(R.id.btnBack);
+        Button btnSend = findViewById(R.id.btnSend);
+        final EditText edtMessage = findViewById(R.id.edtMessage);
 
+        MainActivity.mSocket.on("SERVER_CO_NGUOI_DANG_XUAT", userDangXuatListener);
+        MainActivity.mSocket.on("SERVER_SEND_NEW_USERNAME", newUserJoined);
+        MainActivity.mSocket.on("SERVER_GLOBAL_SEND_MESSAGE", globalMessageListener);
+
+        Intent intent = getIntent();
+        userName = intent.getStringExtra("TEN");
+        names = new ArrayList<>();
+        String namesString = intent.getStringExtra("DANH_SACH");
+        try {
+            JSONArray arrayName = new JSONArray(namesString);
+            for (int i = 0; i < arrayName.length(); i++) {
+                names.add(arrayName.getString(i));
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        lvNames = findViewById(R.id.lvNames);
+        adapterNames = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, names);
+        lvNames.setAdapter(adapterNames);
+
+        lvMessage = findViewById(R.id.lvMessage);
+        messages = new ArrayList<>();
+        adapterMessages = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, messages);
+        lvMessage.setAdapter(adapterMessages);
 
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -20,10 +70,73 @@ public class NoiDungActivity extends AppCompatActivity {
                 onBackPressed();
             }
         });
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String message = edtMessage.getText().toString().trim();
+                if (message.equals("")) return;
+                String jsonObjString = "{\"name\": \"" + userName + "\", \"message\":\"" + message + "\"}";
+                try {
+                    JSONObject messObject = new JSONObject(jsonObjString);
+                    MainActivity.mSocket.emit("USER_GLOBAL_SEND_MESSAGE", messObject);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                edtMessage.setText("");
+            }
+        });
     }
+
+    private Emitter.Listener globalMessageListener = new Emitter.Listener() {
+        @Override
+        public void call(final Object... args) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    JSONObject object = (JSONObject) args[0];
+                    try {
+                        String message = object.getString("name") + ": " + object.getString("message");
+                        messages.add(message);
+                        adapterMessages.notifyDataSetChanged();
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    };
+                }
+            });
+        }
+    };
+
+    private Emitter.Listener newUserJoined = new Emitter.Listener() {
+        @Override
+        public void call(final Object... args) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    String name = (String) args[0];
+                    names.add(name);
+                    adapterNames.notifyDataSetChanged();
+                }
+            });
+        }
+    };
+
+    private Emitter.Listener userDangXuatListener = new Emitter.Listener() {
+        @Override
+        public void call(final Object... args) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    String name = (String) args[0];
+                    names.remove(name);
+                    adapterNames.notifyDataSetChanged();
+                }
+            });
+        }
+    };
 
     @Override
     public void onBackPressed() {
+        MainActivity.mSocket.emit("USER_DANG_XUAT");
         super.onBackPressed();
     }
 }
